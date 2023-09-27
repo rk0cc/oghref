9c39ccc951d19e5f2b133b71baffc3fd5b29ed9b
Cyrus Chan <cyruschan1212@gmail.com>
2023-09-27 13:05:27 +0800

    Add test

--- model/test/model_parse_test.dart
+++ model/test/model_parse_test.dart
@@ -22,6 +22,22 @@ void main() {
         expect(ai.url.toString(), equals("https://example.com/example.wav"));
         expect(ai.type, equals("audio/wav"));
       });
+      test('image', () {
+        ImageInfoParser iip = ImageInfoParser();
+
+        iip
+          ..url = Uri.https("example.com", "/example.wav")
+          ..type = "audio/wav";
+
+        ImageInfo ii = iip.compile();
+
+        iip
+          ..url = Uri.https("example.com", "/example.mp3")
+          ..type = "audio/mp3";
+
+        expect(ii.url.toString(), equals("https://example.com/example.wav"));
+        expect(ii.type, equals("audio/wav"));
+      });
     });
   });
 }
