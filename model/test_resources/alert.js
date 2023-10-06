2de03b63a616d6fc04a3e1dacb7d47de1d7b1130
Cyrus Chan <cyruschan1212@gmail.com>
2023-10-06 23:37:38 +0800

    Edit alert.js for further steps

--- model/test_resources/alert.js
+++ model/test_resources/alert.js
@@ -1,1 +1,15 @@
-alert("This HTML file only for testing purpose in oghref only");
+"use strict";
+
+(() => {
+    alert("This HTML file only for testing purpose in oghref only");
+
+    var ref = document.referrer;
+
+    if (ref != "") {
+        location.replace(ref);
+    } else if (history.length <= 1) {
+        close();
+    } else {
+        history.back();
+    }
+})();
