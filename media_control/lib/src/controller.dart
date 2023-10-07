e2be155dea7d49fa3c24d17ee38518c1c7a6d1ea
Cyrus Chan <cyruschan1212@gmail.com>
2023-10-07 22:07:38 +0800

    Rebuild controller and sequence

--- media_control/lib/src/controller.dart
+++ media_control/lib/src/controller.dart
@@ -1,30 +1,20 @@
 import 'package:flutter/widgets.dart' show ChangeNotifier;
 import 'package:meta/meta.dart';
 
-final class MediaController extends ChangeNotifier {
-  double _duration = 0;
-  bool _isPlaying;
-
-  MediaController({bool autoPlay = false}) : _isPlaying = autoPlay;
-
-  void togglePlayback() {
-    _isPlaying = !_isPlaying;
-    notifyListeners();
-  }
-
-  bool get isPlaying => _isPlaying;
-
-  void _updateDuration(double newDuration) {
-    _duration = newDuration;
-    notifyListeners();
-  }
-
-  double get duration => _duration;
+enum Replay {
+  none,
+  all,
+  once
 }
 
-@internal
-extension MediaDurationUpdater on MediaController {
-  void updateDuration(double newDuration) {
-    _updateDuration(newDuration);
-  }
+@immutable
+final class MediaInfo {
+  final Duration length;
+  final String? label;
+
+  const MediaInfo({required this.length, this.label});
+}
+
+final class MediaPlaybackController extends ChangeNotifier {
+  
 }
--- media_control/lib/src/sequence.dart
+++ media_control/lib/src/sequence.dart
@@ -1,181 +1,45 @@
-import 'package:flutter/widgets.dart' show ChangeNotifier;
-import 'package:http/http.dart'
-    hide delete, get, head, patch, post, put, read, readBytes, runWithClient;
-import 'package:meta/meta.dart';
-import 'package:oghref_builder/oghref_builder.dart' show MetaFetch;
+final class MediaSequenceIterator implements Iterator<Uri> {
+  static const int IDLE_INDEX = -1;
 
-enum ResponseErrorAction { ignore, error }
+  final List<Uri> _sequence;
+  int _currentIndex = IDLE_INDEX;
 
-enum Replay { none, current, allPlaylist }
-
-enum MediaType { audio, video }
-
-@internal
-final class MediaPlaybackIterator extends ChangeNotifier implements Iterator<Uri> {
-  static const int IDLE_CURSOR = -1;
-
-  final List<Uri> _sequence = [];
-  Replay _replay = Replay.none;
-
-  set replay(Replay newReplay) {
-    _replay = newReplay;
-    notifyListeners();
-  }
-
-  Replay get replay => _replay;
-
-  int _cursor = -1;
-  bool _inited = false;
-
-  MediaPlaybackIterator(Iterable<Uri> sequence,
-      {int retry = 3,
-      ResponseErrorAction onResponseError = ResponseErrorAction.error, MediaType? mediaType}) {
-    _assignSequence(sequence, retry, onResponseError, mediaType).then((_) {
-      _inited = true;
-    });
-  }
-
-  Future<void> _assignSequence(Iterable<Uri> sequence, int retry,
-      ResponseErrorAction onResponseError, MediaType? mediaType) async {
-    String? contentPrefix;
-
-    for (Uri seq in sequence) {
-      Request req = Request("GET", seq)
-        ..followRedirects = true
-        ..headers["User-agent"] = MetaFetch.userAgentString;
-
-      late Response resp;
-
-      for (int trial = 0; trial < retry; trial++) {
-        resp = await req.send().then(Response.fromStream);
-
-        if (resp.statusCode < 300) {
-          break;
-        }
-      }
-
-      if (resp.statusCode < 300) {
-        switch (onResponseError) {
-          case ResponseErrorAction.error:
-            throw ClientException("The given Uri responsed with error", seq);
-          case ResponseErrorAction.ignore:
-            continue;
-        }
-      }
-
-      String seqCtxPrefix = resp.headers["content-type"]!.split("/").first;
-
-      if (contentPrefix == null) {
-        contentPrefix = seqCtxPrefix;
-        if (mediaType != null) {
-          final enforcedMediaType = mediaType.name;
-          if (enforcedMediaType != seqCtxPrefix) {
-            throw MediaTypeMismatchedException._(seq, seqCtxPrefix, enforcedMediaType);
-          }
-        }
-      } else if (contentPrefix != seqCtxPrefix) {
-        throw MediaTypeMismatchedException._(seq, seqCtxPrefix, contentPrefix);
-      }
-
-      _sequence.add(seq);
-    }
-  }
-
-  int get cursor {
-    if (_cursor >= _sequence.length || _cursor < 0) {
-      return IDLE_CURSOR;
-    }
-
-    return _cursor;
-  }
-
-  bool get isActive => cursor != IDLE_CURSOR;
-
-  bool get isIdle => cursor == IDLE_CURSOR;
-
-  bool get inited => _inited;
+  MediaSequenceIterator(Iterable<Uri> sequence)
+      : _sequence = List.unmodifiable(sequence);
 
   @override
-  Uri get current {
-    if (!_inited) {
-      throw StateError("Media playback not initalized yet.");
+  Uri get current => _sequence[_currentIndex];
+
+  int get currentIndex {
+    if (_currentIndex >= _sequence.length || _currentIndex < IDLE_INDEX) {
+      return IDLE_INDEX;
     }
 
-    return _sequence[cursor];
+    return _currentIndex;
   }
 
   @override
   bool moveNext() {
-    if (_inited) {
-      if (_cursor < _sequence.length) {
-        _cursor++;
-      }
+    int newIdx = _currentIndex + 1;
 
-      switch (replay) {
-        case Replay.current:
-          if (cursor != IDLE_CURSOR) {
-            break;
-          }
-        case Replay.allPlaylist:
-          if (cursor == IDLE_CURSOR) {
-            _cursor = 0;
-          }
-        case Replay.none:
-          break;
-      }
+    bool inRange = newIdx >= _sequence.length;
 
-      notifyListeners();
+    if (inRange) {
+      _currentIndex++;
     }
 
-    return isActive;
+    return inRange;
   }
 
   bool movePrevious() {
-    if (_inited) {
-      if (cursor > -1) {
-        _cursor--;
-      }
+    int newIdx = _currentIndex - 1;
 
-      switch (replay) {
-        case Replay.current:
-          if (cursor != IDLE_CURSOR) {
-            break;
-          }
-        case Replay.allPlaylist:
-          if (cursor == IDLE_CURSOR) {
-            _cursor = _sequence.length - 18;
-          }
-        case Replay.none:
-          break;
-      }
+    bool inRange = newIdx <= IDLE_INDEX;
 
-      notifyListeners();
+    if (inRange) {
+      _currentIndex--;
     }
 
-    return isActive;
-  }
-}
-
-final class MediaTypeMismatchedException implements Exception {
-  final Uri uri;
-  final String providedType;
-  final String requriedType;
-
-  MediaTypeMismatchedException._(
-      this.uri, this.providedType, this.requriedType);
-
-  @override
-  String toString() {
-    StringBuffer buf = StringBuffer()
-      ..writeln(
-          "MeidaTypeMismatchedException: The given URI resources is not responsed the corresponded type")
-      ..write("\tURI: ")
-      ..writeln(uri)
-      ..write("\tProvided type: ")
-      ..writeln(providedType)
-      ..write("\tRequired type: ")
-      ..writeln(requriedType);
-
-    return buf.toString();
+    return inRange;
   }
 }
