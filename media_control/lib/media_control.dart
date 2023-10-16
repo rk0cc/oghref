e5a4e84bff2158b507789f86a154900879a16d1d
Cyrus Chan <cyruschan1212@gmail.com>
2023-10-17 00:22:03 +0800

    Format

--- media_control/lib/media_control.dart
+++ media_control/lib/media_control.dart
@@ -5,18 +5,21 @@ library media_control;
 import 'package:flutter/cupertino.dart' show DefaultCupertinoLocalizations;
 import 'package:flutter/material.dart' show DefaultMaterialLocalizations;
 import 'package:flutter/widgets.dart'
-    show DefaultWidgetsLocalizations, LocalizationsDelegate, WidgetsFlutterBinding;
+    show
+        DefaultWidgetsLocalizations,
+        LocalizationsDelegate,
+        WidgetsFlutterBinding;
 import 'package:media_kit/media_kit.dart';
 
 export 'src/aspect_ratio.dart' hide AspectRatioWidgetExtension;
 export 'src/player.dart';
 
-/// 
+///
 final class OgHrefMediaControlUnit {
   const OgHrefMediaControlUnit._();
 
   /// Initialize all necessary libraries uses.
-  /// 
+  ///
   /// This must be put after [WidgetsFlutterBinding.ensureInitialized].
   static void ensureInitialized() {
     MediaKit.ensureInitialized();
@@ -24,16 +27,16 @@ final class OgHrefMediaControlUnit {
 
   /// A [List] containts all default [LocalizationsDelegate] for Flutter
   /// provided app theme (Material and Cupertino).
-  /// 
+  ///
   /// Since this library involves Material components, unless there is
   /// [app localization](https://docs.flutter.dev/ui/accessibility-and-internationalization/internationalization#adding-your-own-localized-messages)
   /// implemented into your app, it is required to assign these [LocalizationsDelegate]s
   /// when implement into non-Material app:
-  /// 
+  ///
   /// ```dart
   /// class NonMaterialApp extends StatelessWidget {
   ///   const NonMaterialApp({super.key});
-  /// 
+  ///
   ///   Widget build(BuildContext context) {
   ///     return CupertinoApp(
   ///       localizationDelegates: OgHrefMediaControlUnit.defaultMultiThemeLocalizationDelegates
