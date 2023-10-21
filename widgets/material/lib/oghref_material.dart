6cf7ab7b39bfa12b2049437a90e4a920f781d4c9
Cyrus Chan <cyruschan1212@gmail.com>
2023-10-21 10:04:11 +0800

    Specify export files

--- widgets/material/lib/oghref_material.dart
+++ widgets/material/lib/oghref_material.dart
@@ -1,2 +1,17 @@
 library oghref_material;
 
+import 'package:oghref_builder/oghref_builder.dart';
+import 'package:oghref_media_control/media_control.dart';
+
+export 'package:oghref_builder/oghref_builder.dart';
+export 'src/components/carousel.dart';
+export 'src/widgets/card.dart';
+
+final class OgHrefMaterial {
+  const OgHrefMaterial._();
+
+  static void ensureInitialized() {
+    OgHrefMediaControlUnit.ensureInitialized();
+    MetaFetch().register(const OpenGraphPropertyParser());
+  }
+}
