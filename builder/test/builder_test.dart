d0de67673081eb1925cd3646d744d943f1d55c99
Cyrus Chan <cyruschan1212@gmail.com>
2024-01-04 23:40:42 +0800

    Define mock widget

--- builder/test/builder_test.dart
+++ builder/test/builder_test.dart
@@ -1,22 +1,49 @@
 import 'package:bootleg_url_launcher/bootleg_url_launcher.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:oghref_builder/oghref_builder.dart';
 import 'package:oghref_model/model.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
 
-class TestApp extends StatelessWidget {
-  final Widget child;
+final class MockApp extends StatelessWidget {
+  const MockApp({super.key});
 
-  const TestApp({required this.child, super.key});
+  @override
+  Widget build(BuildContext context) => const MaterialApp(home: MockContext());
+}
+
+final class MockContext extends StatefulWidget {
+  const MockContext({super.key});
+
+  @override
+  State<MockContext> createState() => _MockContextState();
+}
+
+final class _MockContextState extends State<MockContext> {
+  late bool launched;
+  late final TextEditingController ctrl;
+
+  @override
+  void initState() {
+    launched = false;
+    super.initState();
+    ctrl = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    ctrl.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return WidgetsApp(
-        color: const Color(0xF0FEFEFE), home: Center(child: child));
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
+
 
 void main() {
   setUpAll(() {
