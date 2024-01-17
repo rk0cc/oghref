0b6afe6e8f5ef38d25343a1a364394d616ba91a9
Cyrus Chan <cyruschan1212@gmail.com>
2024-01-17 22:45:32 +0800

    New API testing client

--- model/lib/src/client/mock_client.dart
+++ model/lib/src/client/mock_client.dart
@@ -99,21 +99,39 @@ const List<String> _sampleContents = [
 """
 ];
 
+const String _PLAIN_TEXT_MIME = "text/plain";
+
 final Uri _sampleMockHost = Uri.https("127.0.0.2");
 
-/// Simulated environment based on [MockClient] that
-/// all generated content are specified by
-/// tester already.
-///
-/// Setting applied from [MetaFetch.changeTimeout], [MetaFetch.changeUserAgent]
-/// and [MetaFetch.disguiseUserAgent] will also affected in
-/// [MockOgHrefClient], but mostly do not alter [Response.body].
-final class MockOgHrefClient extends BaseClient
+@immutable
+final class MockOgHrefClientContent {
+  final String content;
+  final String contentType;
+
+  const MockOgHrefClientContent(
+      {required this.content, this.contentType = "text/plain"});
+
+  @override
+  int get hashCode =>
+      31 * (content.hashCode ^ (contentType.hashCode >>> 5) - 1);
+
+  @override
+  bool operator ==(Object other) {
+    if (other is MockOgHrefClientContent) {
+      return hashCode == other.hashCode;
+    }
+
+    return false;
+  }
+}
+
+
+final class AdvanceMockOgHrefClient extends BaseClient
     implements MockClient, OgHrefClient {
   @override
   late final Client _c;
 
-  /// Redirect features is always disabled for [MockOgHrefClient].
+  /// Redirect features is always disabled for [AdvanceMockOgHrefClient].
   @override
   bool get redirect => false;
 
@@ -129,6 +147,72 @@ final class MockOgHrefClient extends BaseClient
     return Duration(milliseconds: rand.nextInt(750) + 250);
   }
 
+  AdvanceMockOgHrefClient(Map<Uri, MockOgHrefClientContent> contentLinker,
+      {String errorContentType = _PLAIN_TEXT_MIME}) {
+    if (contentLinker.keys.any((element) => !_isHttpScheme(element))) {
+      throw ArgumentError(
+          "Content linker's URL must be assigned with HTTP(S) scheme.");
+    }
+
+    _c = MockClient((request) {
+      final Map<Uri, MockOgHrefClientContent> ctxLinker =
+          Map.unmodifiable(contentLinker);
+
+      Uri incomingUrl = request.url;
+
+      if (!_isHttpScheme(incomingUrl)) {
+        throw ClientException(
+            "The request should be HTTP(S), eventhough is mock client.");
+      }
+
+      return Future.delayed(_generateResponseDelay(), () {
+        final Map<String, String> errorHeader = {
+          "content-type": errorContentType
+        };
+
+        if (!{"GET", "HEAD"}
+            .any((element) => element == request.method.toUpperCase())) {
+          return Response("", 400, request: request, headers: errorHeader);
+        }
+
+        MockOgHrefClientContent? bodyCtx = ctxLinker[incomingUrl];
+
+        if (bodyCtx == null) {
+          return Response("", 404, headers: errorHeader, request: request);
+        }
+
+        return Response(request.method == "GET" ? bodyCtx.content : "", 200,
+            headers: {"content-type": bodyCtx.contentType}, request: request);
+      });
+    });
+  }
+
+  static bool _isHttpScheme(Uri url) {
+    return RegExp(r"^https?$", caseSensitive: false).hasMatch(url.scheme);
+  }
+
+  @override
+  void close() {
+    _c.close();
+  }
+
+  @override
+  Future<StreamedResponse> send(BaseRequest request) {
+    request
+      ..headers["user-agent"] = OgHrefClient.userAgent
+      ..followRedirects = redirect;
+    return _c.send(request).timeout(Duration(seconds: OgHrefClient.timeoutAt));
+  }
+}
+
+/// Simulated environment based on [MockClient] that
+/// all generated content are specified by
+/// tester already.
+///
+/// Setting applied from [MetaFetch.changeTimeout], [MetaFetch.changeUserAgent]
+/// and [MetaFetch.disguiseUserAgent] will also affected in
+/// [MockOgHrefClient], but mostly do not alter [Response.body].
+final class MockOgHrefClient extends AdvanceMockOgHrefClient {
   /// Define new replicated [Client] for executing under
   /// test environment.
   ///
@@ -150,49 +234,21 @@ final class MockOgHrefClient extends BaseClient
   /// * `text/plain`
   /// * `text/html`
   /// * `application/xhtml+xml`
+  /// 
+  /// At the same time, [contentType] is applied to all content from
+  /// [contentLinker] that consider using [AdvanceMockOgHrefClient.new]
+  /// if difference response content type is needed.
   ///
   /// Moreover, every [Uri] mapped in [contentLinker] **MUST BE** used
   /// `HTTP(S)` protocol. If at least one [Uri.scheme] return other than
   /// `HTTP(S)`, it throws [ArgumentError].
   MockOgHrefClient(Map<Uri, String> contentLinker,
-      {String contentType = "text/plain"}) {
-    if (contentLinker.keys.any((element) => !_isHttpScheme(element))) {
-      throw ArgumentError(
-          "Content linker's URL must be assigned with HTTP(S) scheme.");
-    }
-
-    _c = MockClient((request) {
-      final Map<Uri, String> ctxLinker = Map.unmodifiable(contentLinker);
-
-      Uri incomingUrl = request.url;
-
-      if (!_isHttpScheme(incomingUrl)) {
-        throw ClientException(
-            "The request should be HTTP(S), eventhough is mock client.");
-      }
-
-      return Future.delayed(_generateResponseDelay(), () {
-        final Map<String, String> headers = Map.unmodifiable({
-          "content-type": contentType,
-          "user-agent": OgHrefClient.userAgent
+      {String contentType = "text/plain"})
+      : super({
+          for (var MapEntry(key: url, value: body) in contentLinker.entries)
+            url:
+                MockOgHrefClientContent(content: body, contentType: contentType)
         });
-
-        if (!{"GET", "HEAD"}
-            .any((element) => element == request.method.toUpperCase())) {
-          return Response("", 400, request: request, headers: headers);
-        }
-
-        String? bodyCtx = ctxLinker[incomingUrl];
-
-        if (bodyCtx == null) {
-          return Response("", 404, headers: headers, request: request);
-        }
-
-        return Response(request.method == "GET" ? bodyCtx : "", 200,
-            headers: headers, request: request);
-      });
-    });
-  }
 
   /// Uses [sample files](https://github.com/rk0cc/oghref/tree/main/model/sample) to defined
   /// content of the simulated HTML files with hosted IP address as `127.0.0.2` with `HTTPS`
@@ -201,21 +257,4 @@ final class MockOgHrefClient extends BaseClient
         for (int idx = 0; idx < _sampleContents.length; idx++)
           _sampleMockHost.resolve("${idx + 1}.html"): _sampleContents[idx]
       });
-
-  static bool _isHttpScheme(Uri url) {
-    return RegExp(r"^https?$", caseSensitive: false).hasMatch(url.scheme);
-  }
-
-  @override
-  void close() {
-    _c.close();
-  }
-
-  @override
-  Future<StreamedResponse> send(BaseRequest request) {
-    request
-      ..headers["user-agent"] = OgHrefClient.userAgent
-      ..followRedirects = redirect;
-    return _c.send(request).timeout(Duration(seconds: OgHrefClient.timeoutAt));
-  }
 }
